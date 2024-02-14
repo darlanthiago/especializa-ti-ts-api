318a84984efbf95ddc4b3e530571f8d755d7cfff
Darlan Thiago <tdarlan1@hotmail.com>
2024-02-14 09:16:58 -0300

    configure express and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Started");
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+
+const PORT = process.env.API_PORT || 3000;
+
+const app = express();
+
+app.get("/", (request: Request, response: Response) => {
+  return response.send("SERVER UP");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
